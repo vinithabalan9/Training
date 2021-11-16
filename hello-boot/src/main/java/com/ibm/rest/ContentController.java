52d7d20decff5e631db724d5de1fb037758a57c0
Vinitha k <vinithabalan9@gmail.com>
2021-11-16 13:19:09 +0530

    Hello Boot

---
+++ hello-boot/src/main/java/com/ibm/rest/ContentController.java
@@ -0,0 +1,35 @@
+package com.ibm.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
+@RestController
+public class ContentController {
+	
+	@GetMapping(value = "/greet/{name}", produces = "text/plain")
+	public String greetText(@PathVariable String name) {
+		return "welcome to spring boot, "+name;
+	}
+	
+	@GetMapping(value = "/greet", produces = "text/html")
+	public String greetHtml(@RequestParam String name) {
+		return "<h1>welcome to spring boot,"+name+" </h1>";
+	}
+	
+	@GetMapping(value= "/greet", produces = "text/xml")
+	public String greetXml() {
+		return "<?xml version=\"1.0\"><greeting>Welcome to spring Boot</greeting>";
+	}
+	
+	@GetMapping(value= "/greet", produces = "application/json")
+	public String greetJson() {
+		JSONPObject obj = new JSONPObject("greeting", "Welcome to Spring Boot");
+		return obj.toString();
+	}
+	
+
+}
